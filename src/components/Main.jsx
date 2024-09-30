b7b6c197d9747fc9f59c9597a6af18b22542736b
GOKUL <gokuln2004@gmail.com>
2024-09-30 13:30:53 +0530

    ViewAI Landing Page

---
+++ src/components/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Home from './Home'
+
+export default function Main() {
+
+  return (
+
+    <div className='overflow-hidden flex flex-col justify-center items-center bg-current font-[Urbanist]'>
+        <Home />
+    </div>
+    
+  )
+}
